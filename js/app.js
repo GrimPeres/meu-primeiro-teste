2814f8ff35ccf78822ac2c2c44ddc29725f6eb76
GrimPeres <grim_alexandre@hotmail.com>
2026-07-04 23:39:23 +0100

    1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("company-name").textContent = config.company.name;
+
+    document.getElementById("company-slogan").textContent = config.company.slogan;
+
+});
---
+++ js/config.js
@@ -0,0 +1,19 @@
+const config = {
+    company: {
+        name: "A Minha Empresa",
+        slogan: "Catálogos Digitais"
+    },
+
+    catalogs: [
+        {
+            title: "Catálogo Principal",
+            pdf: "pdf/catalogo.pdf"
+        }
+    ],
+
+    contacts: {
+        website: "https://www.exemplo.pt",
+        email: "geral@exemplo.pt",
+        phone: "+351 912345678"
+    }
+};
